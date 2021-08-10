401a5b9b5ee18a6ca79a84975373514cac6212d1
Dev <dev@pryal.com.br>
2021-08-10 00:57:49 -0300

    apiservice

--- src/core/services/ApiService.ts
+++ src/core/services/ApiService.ts
@@ -19,10 +19,10 @@ class ApiService {
   public static init(app: App<Element>) {
     ApiService.vueInstance = app;
     ApiService.vueInstance.use(VueAxios, axios);
-    ApiService.vueInstance.axios.defaults.baseURL =
-      "https://localhost:5001/api/";
     // ApiService.vueInstance.axios.defaults.baseURL =
-    //   "https://pryal-back-homol.azurewebsites.net/api/";
+    //   "https://localhost:5001/api/";
+    ApiService.vueInstance.axios.defaults.baseURL =
+      "https://pryal-back-homol.azurewebsites.net/api/";
     ApiService.vueInstance.axios.defaults.headers.common["Accept"] =
       "application/json";
     ApiService.vueInstance.axios.defaults.headers.common["Content-Type"] =
